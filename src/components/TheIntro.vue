0e5db503276f3182eba0a94c548ffc08a94ef84b
“Marco <marco.pianaroli96@gmail.com>
2024-02-03 15:36:22 +0100

    removes contact info from intro

--- src/components/TheIntro.vue
+++ src/components/TheIntro.vue
@@ -1,6 +1,6 @@
 <script setup lang='ts'>
 import { ref } from 'vue';
-import PillButton from '@/components/PillButton.vue';
+
 
 const info = ref(
   {
@@ -25,20 +25,13 @@ const info = ref(
 </script>
 
 <template>
-  <div class="flex  text-white justify-center items-center">
+  <div class="flex flex-col justify-center text-white  mt-20 ">
     <Icon icon="carbon:logo-linkedin" />
-    <img src="../assets/marcoprofile.jpg" class="h-24 w-24 mx-2 rounded-full object-cover" />
-    <div class="flex flex-col text-white justify-start">
-      <p class="text-3xl">{{ info.title }} {{ info.name }}</p>
-      <p class="text-3xl"> I'm a <span class="text-3xl text-blue-500"> {{ info.role }} </span> developer</p>
+    <img src="../assets/marcoprofile.jpg" class="h-24 w-24 mx-2 self-center rounded-full object-cover" />
+    <div class="flex flex-col text-white justify-start items-center">
+      <p class="text-6xl">{{ info.title }} {{ info.name }}</p>
+      <p class="text-5xl"> I'm a <span class="text-5xl text-blue-500"> {{ info.role }} </span> developer</p>
 
-    </div>
-
-  </div>
-  <div class="flex flex-col text-white justify-center items-center">
-    <span class="text-xl w-3xl p-3 ">{{ info.description }}</span>
-    <div class="flex flex-row justify-center items-center">
-      <PillButton v-for="link in info.links" :key="link.name" :icon="link.icon" :text="link.name" :url="link.url" />
     </div>
   </div>
 </template>
