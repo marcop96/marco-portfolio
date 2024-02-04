b3459dd19ee0dbff2fe883bfc62fc0fbeda4866c
“Marco <marco.pianaroli96@gmail.com>
2024-02-04 18:34:44 +0100

    chore: adds blur filter to navbar

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -3,8 +3,8 @@
 </script>
 
 <template>
-  <div class="flex justify-center p-7 ">
-    <nav class="flex flex-row color-white border-2 rounded-lg border-white w-fit h-fit p-2 ">
+  <div class="flex justify-center p-7">
+    <nav class="flex flex-row color-white border-2 rounded-lg border-white w-fit h-fit p-2 fixed backdrop-blur ">
       <p class="p-2" hover:bg-blue-500 p-3 rounded-xl cursor-pointer>projects</p>
       <p class="p-2" hover:bg-blue-500 p-3 rounded-xl cursor-pointer>experience</p>
       <p class="p-2" hover:bg-blue-500 p-3 rounded-xl cursor-pointer>about me</p>
