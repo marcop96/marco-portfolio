26740f56004a5d199812773906d70e4810811bec
“Marco <marco.pianaroli96@gmail.com>
2024-02-04 18:34:29 +0100

    chore: adds interactivity to pillbuttons

--- src/components/TheProjects.vue
+++ src/components/TheProjects.vue
@@ -86,14 +86,14 @@ const projects: Project[] = [{
       <p class="text-2xl w-fit ">{{ project.title }}</p>
       <div class="flex space-x-2 justify-center w-fit">
         <PillButton v-for="technology in project.technologies" :key="technology" :icon="technology"
-          styles="bg-white rounded-xl " />
+          styles="bg-white rounded-xl hover:bg-dark hover:text-white " />
       </div>
       <p class=" my-2  max-w-96  overscroll-auto">{{ project.description }}</p>
 
 
       <div class="flex justify-end ">
         <PillButton v-for="link in project.links" :key="link.name" :url="link.url" :icon="link.icon"
-          styles="bg-white text-black  h-fit align-bottom rounded-xl mx-1 w-fit " />
+          styles="bg-white text-black  h-fit align-bottom rounded-xl mx-1 w-fit hover:bg-dark hover:text-white " />
       </div>
     </div>
 
