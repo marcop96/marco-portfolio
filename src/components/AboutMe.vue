a1547c616d14cf6f1ca2fe9bfe95864e90279cd2
“Marco <marco.pianaroli96@gmail.com>
2024-02-07 18:35:55 +0100

    creates aboutme component

---
+++ src/components/AboutMe.vue
@@ -0,0 +1,21 @@
+<script setup lang='ts'>
+</script>
+
+<template>
+  <div class="text-white mt-16 mx-21 justify-start">
+    <h1 class="text-4xl font-bold mb-8">Hi, I'm Marco Pianaroli</h1>
+    <p class="text-lg mb-6">Welcome to my world of creativity and innovation!</p>
+    <p class="text-lg mb-6">As a front-end developer, I combine my passion for gaming with my expertise in web development
+      to create immersive and interactive experiences.</p>
+    <p class="text-lg mb-6">With VueJS as my weapon of choice, I craft responsive and dynamic web applications that
+      captivate users and solve real-world problems.</p>
+    <p class="text-lg mb-6">But I'm not just a developer. I'm also a sports enthusiast, always striving for teamwork,
+      discipline, and perseverance—qualities that I bring to every project I undertake.</p>
+    <p class="text-lg mb-6 ">Join me on this exciting journey as we explore the vast
+      landscape of
+      front-end
+      development and
+      make a meaningful impact in the digital world.</p>
+
+  </div>
+</template>
