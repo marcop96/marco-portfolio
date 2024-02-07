ba36eba229d757355e9a375f6b5d7dcdc1b0e79d
“Marco <marco.pianaroli96@gmail.com>
2024-02-07 18:36:33 +0100

    chore: fix eslint attributes

--- eslint.config.js
+++ eslint.config.js
@@ -4,5 +4,7 @@ export default await antfu({
   rules: {
     'antfu/top-level-function': 'off',
     'vue/singleline-html-element-content-newline': 'off',
+    'vue/first-attribute-linebreak': 'off',
+    'vue/html-self-closing': 'off',
   },
 })
