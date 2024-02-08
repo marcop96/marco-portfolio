f55dcfa656f5061db450d914f668a35e43803f5d
“Marco <marco.pianaroli96@gmail.com>
2024-02-08 16:58:23 +0100

    bugfix: header empty routers

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -6,9 +6,8 @@
   <div class="flex justify-center p-7">
     <nav class="flex flex-row color-white border-2 rounded-lg border-white w-fit h-fit p-2 fixed top-4 backdrop-blur">
       <router-link to="projects" class="p-2">projects</router-link>
-      <router-link to="experience" class="p-2">experience</router-link>
       <router-link to="about" class="p-2">about me</router-link>
-      <router-link to="contact" class="p-2">contact</router-link>
+      <router-link to="contactme" class="p-2">contact</router-link>
 
 
     </nav>
