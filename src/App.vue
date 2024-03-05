99a187ee5adc6ee695e33506af77ebce1ccbc757
“Marco <marco.pianaroli96@gmail.com>
2024-03-05 21:52:39 +0100

    Refactor import statements and update route meta data

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,6 @@
 <script setup lang="ts">
-import { RouterView } from 'vue-router';
-import TheHeader from '@/components/TheHeader.vue';
-
-
+import { RouterView } from "vue-router";
+import TheHeader from "@/components/TheHeader.vue";
 </script>
 
 <template>
--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
-
-
+import { createRouter, createWebHistory } from "vue-router";
 
 // Update once you need routes
 // Remember to import over at `main.ts` by adding
@@ -8,30 +6,35 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
   {
-    path: '/',
-    component: () => import('./views/HomePage.vue'),
+    path: "/",
+    component: () => import("./views/HomePage.vue"),
+    meta: { transition: "slide-left" },
   },
   {
-    path: '/about',
-    component: () => import('./views/AboutMe.vue'),
+    path: "/about",
+    component: () => import("./views/AboutMe.vue"),
+    meta: { transition: "fade" },
   },
   {
-    path: '/contactme',
-    component: () => import('./views/ContactMe.vue'),
+    path: "/contactme",
+    component: () => import("./views/ContactMe.vue"),
+    meta: { transition: "slide-right" },
   },
   {
-    path: '/projects',
-    component: () => import('./views/TheProjects.vue'),
+    path: "/projects",
+    component: () => import("./views/TheProjects.vue"),
+    meta: { transition: "zoom" },
   },
   {
-    path: '/:pathMatch(.*)*',
-    component: () => import('./views/NotFound.vue'),
+    path: "/:pathMatch(.*)*",
+    component: () => import("./views/NotFound.vue"),
+    meta: { transition: "fade" },
   },
-]
+];
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
-})
+});
 
-export default router
+export default router;
